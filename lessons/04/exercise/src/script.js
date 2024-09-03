6d11d5742dbff9a9073615e1340f1b6a1759e4c7
naejbravo <naejbravo@gmail.com>
2024-09-04 00:33:52 +0200

    06

--- lessons/04/exercise/src/script.js
+++ lessons/04/exercise/src/script.js
@@ -6,16 +6,40 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+//group
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.x = 0.7
-mesh.position.y = -0.6
-mesh.position.z = 1
-scene.add(mesh)
+
+cube1.position.set(0,0,0);
+cube2.position.set(-2, 0,0);
+cube3.position.set(2, 0,0);
+
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
 
 /**
  * Sizes
@@ -24,6 +48,10 @@ const sizes = {
     width: 800,
     height: 600
 }
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 /**
  * Camera
